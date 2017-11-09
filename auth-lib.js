66a9646e357e71f2145deb241cfc76419fe7c619
Kocherov Kirill <0KypT0@gmail.com>
2017-11-09 20:05:49 +0500

    Create auth-lib.js

---
+++ auth-lib.js
@@ -0,0 +1,24 @@
+var allUsers = [];
+
+function users() { return allUsers }
+
+function createUser(name, pass) { 
+	var user = {name: name, pass: pass};
+	allUsers.push(user);
+	return user;
+ }
+
+var noop = function() {}
+var deleteUser = noop;
+var userGroups = noop;
+var addUserToGroup = noop;
+var removeUserFromGroup = noop;
+var rights = noop;
+var createRight = noop;
+var deleteRight = noop;
+var addRightToGroup = noop;
+var removeRightFromGroup = noop;
+var groups = noop;
+var createGroup = noop;
+var deleteGroup = noop;
+var groupRights = noop;
